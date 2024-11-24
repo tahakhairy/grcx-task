150497037621712623039cb1b164b794a3ab3539
taha-khairy <taha.khairy@dphish.com>
2024-11-24 20:23:59 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/index.css'
 import router from './router/index'
-import axios from 'axios'
-
-axios.defaults.baseURL = 'http://139.162.137.167/'
 
 const app = createApp(App)
 app.use(router)
